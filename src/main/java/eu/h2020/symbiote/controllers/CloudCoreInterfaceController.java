11fd60ebe0951c4f3ed26ed0d8dd972483c56199
Artur Jaworski <arturj@man.poznan.pl>
2017-04-18 14:41:47 +0200

    Resource creation/modification/removal for basic and rdf requests

--- src/main/java/eu/h2020/symbiote/controllers/CloudCoreInterfaceController.java
+++ src/main/java/eu/h2020/symbiote/controllers/CloudCoreInterfaceController.java
@@ -11,7 +11,6 @@ import eu.h2020.symbiote.core.internal.CoreResourceRegistryRequest;
 import eu.h2020.symbiote.core.internal.CoreResourceRegistryResponse;
 import eu.h2020.symbiote.core.internal.DescriptionType;
 import eu.h2020.symbiote.core.model.resources.Resource;
-import eu.h2020.symbiote.model.ResourceRegistryResponseWithStatus;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +24,16 @@ import java.util.List;
 /**
  * Class defining all REST endpoints.
  * <p>
- * CloudCoreInterface, as the name suggests, is just an interface, therefore it forwards all requests to modules responsible
- * for handling them via RabbitMQ.
+ * CloudCoreInterface, as the name suggests, is just an interface, therefore it forwards all requests to
+ * modules responsible for handling them via RabbitMQ.
  */
 @RestController
 public class CloudCoreInterfaceController {
     private static final String URI_PREFIX = "/cloudCoreInterface/v1";
 
     public static Log log = LogFactory.getLog(CloudCoreInterfaceController.class);
+
+    private enum CoreOperationType {CREATE, MODIFY, DELETE}
 
     private final RabbitManager rabbitManager;
 
@@ -46,17 +47,82 @@ public class CloudCoreInterfaceController {
         this.rabbitManager = rabbitManager;
     }
 
-    private ResourceRegistryResponseWithStatus handleResourceCreationRequest(CoreResourceRegistryRequest coreResourceRegistryRequest) {
+    private CoreResourceRegistryRequest prepareRdfRequest(String platformId, RDFResourceRegistryRequest resourceRegistryRequest, String token) {
+        CoreResourceRegistryRequest coreRequest = new CoreResourceRegistryRequest();
+        coreRequest.setToken(token);
+        coreRequest.setDescriptionType(DescriptionType.RDF);
+        coreRequest.setPlatformId(platformId);
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+
+            String resourcesJson = mapper.writeValueAsString(resourceRegistryRequest.getRdfInfo());
+            coreRequest.setBody(resourcesJson);
+
+            log.debug("Request for Core Services prepared");
+
+        } catch (JsonProcessingException e) {
+            log.error("Error while handling resource creation request", e);
+            return null;
+        }
+
+        return coreRequest;
+    }
+
+    private CoreResourceRegistryRequest prepareBasicRequest(String platformId, ResourceRegistryRequest resourceRegistryRequest, String token) {
+        CoreResourceRegistryRequest coreRequest = new CoreResourceRegistryRequest();
+        coreRequest.setToken(token);
+        coreRequest.setDescriptionType(DescriptionType.BASIC);
+        coreRequest.setPlatformId(platformId);
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+
+            String resourcesJson = mapper.writerFor(new TypeReference<List<Resource>>() {
+            }).writeValueAsString(resourceRegistryRequest.getResources());
+            coreRequest.setBody(resourcesJson);
+
+            log.debug("Request for Core Services prepared");
+
+
+        } catch (JsonProcessingException e) {
+            log.error("Error while handling resource creation request", e);
+            return null;
+        }
+
+        return coreRequest;
+    }
+
+    private ResponseEntity<?> handleCoreResourceRequest(CoreResourceRegistryRequest coreResourceRegistryRequest, CoreOperationType coreOperationType) {
+        ResourceRegistryResponse response = new ResourceRegistryResponse();
+
+        if (coreResourceRegistryRequest == null) {
+            log.error("Error while handling resource request");
+            response.setMessage("Error while parsing message body. No data has been processed by Core Services.");
+            response.setResources(null);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+
         log.debug("Sending request to Core Services: " + coreResourceRegistryRequest.getBody());
-        CoreResourceRegistryResponse coreResponse = rabbitManager.sendResourceCreationRequest(coreResourceRegistryRequest);
+        CoreResourceRegistryResponse coreResponse = null;
+        switch (coreOperationType) {
+            case CREATE:
+                coreResponse = rabbitManager.sendResourceCreationRequest(coreResourceRegistryRequest);
+                break;
+            case MODIFY:
+                coreResponse = rabbitManager.sendResourceModificationRequest(coreResourceRegistryRequest);
+                break;
+            case DELETE:
+                coreResponse = rabbitManager.sendResourceRemovalRequest(coreResourceRegistryRequest);
+                break;
+        }
 
         //Timeout or exception on our side
         if (coreResponse == null) {
             log.debug("Timeout on handling request by Core Services");
-            ResourceRegistryResponseWithStatus responseWithStatus = new ResourceRegistryResponseWithStatus();
-            responseWithStatus.setStatus(500);
-            responseWithStatus.getResourceRegistryResponse().setMessage("Timeout on Core Services side. Resources might have been created, but response did not arrive on time.");
-            return responseWithStatus;
+            response.setMessage("Timeout on Core Services side. Operation might have been performed, but response did not arrive on time.");
+            response.setResources(null);
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
         log.debug("Response from Core Services received: " + coreResponse.getStatus() + ", " + coreResponse.getMessage() + ", " + coreResponse.getBody());
@@ -70,21 +136,20 @@ public class CloudCoreInterfaceController {
                 });
             } catch (IOException e) {
                 log.error("Error while parsing response body from Core Services", e);
-                ResourceRegistryResponseWithStatus responseWithStatus = new ResourceRegistryResponseWithStatus();
-                responseWithStatus.setStatus(500);
-                responseWithStatus.getResourceRegistryResponse().setMessage("Error while parsing response body from Core Service. Resources might have been vreated, but response was malformed.");
-                return responseWithStatus;
+                response.setMessage("Error while parsing response body from Core Service. Operation might have been performed, but response was malformed.");
+                response.setResources(null);
+                return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
 
-        ResourceRegistryResponse response = new ResourceRegistryResponse();
         response.setMessage(coreResponse.getMessage());
         response.setResources(responseListOfResources);
 
         log.debug("ResourceRegistryResponse created and returned to endpoint");
 
-        return new ResourceRegistryResponseWithStatus(coreResponse.getStatus(), response);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(coreResponse.getStatus()));
     }
+
 
     /**
      * Endpoint for creating resources using RDF description.
@@ -101,32 +166,8 @@ public class CloudCoreInterfaceController {
                                                 @RequestHeader("Authorization") String token) {
         log.debug("Request for creation of RDF resources");
 
-        CoreResourceRegistryRequest coreRequest = new CoreResourceRegistryRequest();
-        coreRequest.setToken(token);
-        coreRequest.setDescriptionType(DescriptionType.RDF);
-        coreRequest.setPlatformId(platformId);
-
-        ResourceRegistryResponseWithStatus response;
-
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-
-            String resourcesJson = mapper.writeValueAsString(resourceRegistryRequest.getRdfInfo());
-            coreRequest.setBody(resourcesJson);
-
-            log.debug("Request for Core Services prepared");
-
-            response = handleResourceCreationRequest(coreRequest);
-
-        } catch (JsonProcessingException e) {
-            log.error("Error while handling resource creation request", e);
-            response = new ResourceRegistryResponseWithStatus();
-            response.getResourceRegistryResponse().setMessage("Error while parsing message body. No data has been processed by Core Services.");
-            response.setStatus(400);
-        }
-
-        log.debug("Returning response to client");
-        return new ResponseEntity<>(response.getResourceRegistryResponse(), HttpStatus.valueOf(response.getStatus()));
+        CoreResourceRegistryRequest coreRequest = prepareRdfRequest(platformId, resourceRegistryRequest, token);
+        return handleCoreResourceRequest(coreRequest, CoreOperationType.CREATE);
     }
 
     /**
@@ -135,11 +176,14 @@ public class CloudCoreInterfaceController {
      * Currently not implemented.
      */
     @RequestMapping(method = RequestMethod.PUT,
-            value = URI_PREFIX + "/platforms/{platformId}/rdfResources/{resourceId}")
+            value = URI_PREFIX + "/platforms/{platformId}/rdfResources")
     public ResponseEntity<?> modifyRdfResource(@PathVariable("platformId") String platformId,
-                                               @PathVariable("resourceId") String resourceId,
-                                               @RequestBody String rdfResources) {
-        return new ResponseEntity<>("RDF Resource modify: NYI", HttpStatus.NOT_IMPLEMENTED);
+                                               @RequestBody RDFResourceRegistryRequest resourceRegistryRequest,
+                                               @RequestHeader("Authorization") String token) {
+        log.debug("Request for modification of RDF resources");
+
+        CoreResourceRegistryRequest coreRequest = prepareRdfRequest(platformId, resourceRegistryRequest, token);
+        return handleCoreResourceRequest(coreRequest, CoreOperationType.MODIFY);
     }
 
     /**
@@ -148,10 +192,15 @@ public class CloudCoreInterfaceController {
      * Currently not implemented.
      */
     @RequestMapping(method = RequestMethod.DELETE,
-            value = URI_PREFIX + "/platforms/{platformId}/rdfResources/{resourceId}")
+            value = URI_PREFIX + "/platforms/{platformId}/rdfResources")
     public ResponseEntity<?> deleteRdfResource(@PathVariable("platformId") String platformId,
-                                               @PathVariable("resourceId") String resourceId) {
-        return new ResponseEntity<>("RDF Resource delete: NYI", HttpStatus.NOT_IMPLEMENTED);
+                                               @RequestBody RDFResourceRegistryRequest resourceRegistryRequest,
+                                               @RequestHeader("Authorization") String token) {
+        log.debug("Request for removal of RDF resources");
+
+        CoreResourceRegistryRequest coreRequest = prepareRdfRequest(platformId, resourceRegistryRequest, token);
+        return handleCoreResourceRequest(coreRequest, CoreOperationType.DELETE);
+
     }
 
     /**
@@ -169,87 +218,48 @@ public class CloudCoreInterfaceController {
                                              @RequestHeader("Authorization") String token) {
         log.debug("Request for creation of basic resources");
 
-        CoreResourceRegistryRequest coreRequest = new CoreResourceRegistryRequest();
-        coreRequest.setToken(token);
-        coreRequest.setDescriptionType(DescriptionType.BASIC);
-        coreRequest.setPlatformId(platformId);
-
-        ResourceRegistryResponseWithStatus response;
-
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-
-            String resourcesJson = mapper.writerFor(new TypeReference<List<Resource>>() {
-            }).writeValueAsString(resourceRegistryRequest.getResources());
-            coreRequest.setBody(resourcesJson);
-
-            log.debug("Request for Core Services prepared");
-
-            response = handleResourceCreationRequest(coreRequest);
-
-        } catch (JsonProcessingException e) {
-            log.error("Error while handling resource creation request", e);
-            response = new ResourceRegistryResponseWithStatus();
-            response.getResourceRegistryResponse().setMessage("Error while parsing message body. No data has been processed by Core Services.");
-            response.setStatus(400);
-        }
-
-        log.debug("Returning response to client");
-        return new ResponseEntity<>(response.getResourceRegistryResponse(), HttpStatus.valueOf(response.getStatus()));
+        CoreResourceRegistryRequest coreRequest = prepareBasicRequest(platformId, resourceRegistryRequest, token);
+        return handleCoreResourceRequest(coreRequest, CoreOperationType.CREATE);
     }
 
     /**
      * Endpoint for modifying resource using JSON description.
      *
-     * @param platformId ID of a platform that resource belongs to; if platform ID is specified in Resource body object,
-     *                   it will be overwritten by path parameter
-     * @param resourceId ID of a resource to modify; if resource ID is specified in Resource body object,
-     *                   it will be overwritten by path parameter
-     * @param resource   resource that is to be modified
+     * @param platformId              ID of a platform that resource belongs to; if platform ID is specified in Resource body object,
+     *                                it will be overwritten by path parameter
+     * @param resourceRegistryRequest request containing resources to be modified
+     * @param token                   authorization token
      * @return modified resource or null along with appropriate error HTTP status code
      */
-//    @RequestMapping(method = RequestMethod.PUT,
-//            value = URI_PREFIX + "/platforms/{platformId}/resources/{resourceId}")
-//    public ResponseEntity<?> modifyResource(@PathVariable("platformId") String platformId,
-//                                            @PathVariable("resourceId") String resourceId,
-//                                            @RequestBody Resource resource) {
-//        resource.setPlatformId(platformId);
-//        resource.setId(resourceId);
-//        RpcResourceResponse response = rabbitManager.sendResourceModificationRequest(resource);
-//
-//        log.debug(response);
-//
-//        //Timeout or exception on our side
-//        if (response == null)
-//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//
-//        return new ResponseEntity<>(response.getResource(), HttpStatus.valueOf(response.getStatus()));
-//    }
+    @RequestMapping(method = RequestMethod.PUT,
+            value = URI_PREFIX + "/platforms/{platformId}/resources")
+    public ResponseEntity<?> modifyResource(@PathVariable("platformId") String platformId,
+                                            @RequestBody ResourceRegistryRequest resourceRegistryRequest,
+                                            @RequestHeader("Authorization") String token) {
+        log.debug("Request for modification of basic resources");
+
+        CoreResourceRegistryRequest coreRequest = prepareBasicRequest(platformId, resourceRegistryRequest, token);
+        return handleCoreResourceRequest(coreRequest, CoreOperationType.MODIFY);
+    }
 
     /**
      * Endpoint for removing resource using JSON description.
      *
-     * @param platformId ID of a platform that resource belongs to
-     * @param resourceId ID of a resource to remove
+     * @param platformId              ID of a platform that resource belongs to
+     * @param resourceRegistryRequest request containing resources to be removed
+     * @param token                   authorization token
      * @return empty body with appropriate operation HTTP status code
      */
-//    @RequestMapping(method = RequestMethod.DELETE,
-//            value = URI_PREFIX + "/platforms/{platformId}/resources/{resourceId}")
-//    public ResponseEntity<?> deleteResource(@PathVariable("platformId") String platformId,
-//                                            @PathVariable("resourceId") String resourceId) {
-//        Resource resource = new Resource();
-//        resource.setId(resourceId);
-//        resource.setPlatformId(platformId);
-//        RpcResourceResponse response = rabbitManager.sendResourceRemovalRequest(resource);
-//
-//        log.debug(response);
-//
-//        //Timeout or exception on our side
-//        if (response == null)
-//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//
-//        return new ResponseEntity<>(null, HttpStatus.valueOf(response.getStatus()));
-//    }
+    @RequestMapping(method = RequestMethod.DELETE,
+            value = URI_PREFIX + "/platforms/{platformId}/resources")
+    public ResponseEntity<?> deleteResource(@PathVariable("platformId") String platformId,
+                                            @RequestBody ResourceRegistryRequest resourceRegistryRequest,
+                                            @RequestHeader("Authorization") String token) {
+        log.debug("Request for removal of basic resources");
+
+        CoreResourceRegistryRequest coreRequest = prepareBasicRequest(platformId, resourceRegistryRequest, token);
+        return handleCoreResourceRequest(coreRequest, CoreOperationType.DELETE);
+    }
 
 
 }
